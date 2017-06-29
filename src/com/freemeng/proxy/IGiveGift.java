511aff3e63fb1d04c1dee84617d9277f614506da
zmdstr <zhenyang0510@163.com>
2017-06-29 09:08:33 +0800

    Proxy Partten

---
+++ src/com/freemeng/proxy/IGiveGift.java
@@ -0,0 +1,13 @@
+package com.freemeng.proxy;
+
+/**
+ * @ClassName:IGiveGift
+ * @Description:TODO
+ * @author: zhangzhenyang
+ * @time:2017年6月29日 上午8:50:41
+ */
+public interface IGiveGift {
+	void giveDolls();
+	void giveFlowers();
+	void giveChoolate();
+}
